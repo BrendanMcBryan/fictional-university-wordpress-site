c7ca247f3abb08c52ca57d700750138eaf890491
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-05-21 15:37:42 -0700

    Set up JS File

---
+++ plugins/are-you-paying-attention/index.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+  Plugin Name: Are you paying attention
+  Description: ger Reader Multiple Choice Questions.
+  Version: 1.0
+  Author: Brendan
+  Author URI: https://www.designingintent.com/
+
+*/
+
+
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+class AreYouPayingAttention
+{
+  function __construct()
+  {
+    add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+  }
+
+  function adminAssets()
+  {
+    wp_enqueue_script('ournewblocktype', plugin_dir_url(__FILE__) . 'test.js', array('wp-blocks', 'wp-element'));
+  }
+}
+
+$areYouPayingAttention = new AreYouPayingAttention();
