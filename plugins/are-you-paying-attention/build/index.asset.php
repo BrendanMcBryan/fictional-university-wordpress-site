b71a1026cda2747d02c57f69265ef613114a8d22
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-05-22 08:36:18 -0700

    Set UP JSX for return

---
+++ plugins/are-you-paying-attention/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react'), 'version' => '0cce83aeb6312b870796');
--- plugins/are-you-paying-attention/src/index.js
+++ plugins/are-you-paying-attention/src/index.js
@@ -3,13 +3,13 @@ wp.blocks.registerBlockType('ourplugin/are-you-paying-attention', {
   icon: 'smiley',
   category: 'common',
   edit: function () {
-    return wp.element.createElement(
-      'h3',
-      null,
-      'Hello, this is from the admin editor screen.'
+    return (
+      <div>
+        <h3>This is an h3 from JSX homie. </h3>
+      </div>
     );
   },
   save: function () {
-    return wp.element.createElement('h1', null, 'This is the frontend.');
+    return wp.element.createElement('h1', null, 'This is front.');
   },
 });
