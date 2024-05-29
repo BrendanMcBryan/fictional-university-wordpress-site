d0460442eb7f22ebdc8db9bdc782f81674144b54
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-05-29 13:30:41 -0700

    Complete banner block

--- themes/fictional-block-theme/build/banner.asset.php
+++ themes/fictional-block-theme/build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '0f23a47b6951207ea869');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '1c0da49d88553654a7ea');
